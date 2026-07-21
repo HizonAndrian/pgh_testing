08b82a02f6a068845d194b8df82b428f5ad94417
[HizonAndrian] <[markandrianhizon@gmail.com]>
2026-07-21 18:31:56 +0800

    Revert "Initial Commit"
    This reverts commit 5369fcda0551f6b920e82caf562d43fedfa73cb7.

--- list_comprehension.py
+++
@@ -1,8 +0,0 @@
-# List comprehension = A concise way to create lists in Python
-#                      Compact and Easier to read than traditional loops
-#                      [expression for value in iterable if condition]
-
-num_list = [1, 2 , 4, 5]
-name_list = ["andrian", "kesian"]
-doubles = [x[0].upper() + x[1:] for x in name_list if x == "andrian"]
-print(doubles)
